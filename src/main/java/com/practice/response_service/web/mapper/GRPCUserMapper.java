30de8769cc7e81b729ba2ba539d205e18a427084
Metod-OverDrive <pochta.nadomne@mail.ru>
2023-12-25 00:55:10 +0300

    #7 Add Docker compose

--- src/main/java/com/practice/response_service/web/mapper/GRPCUserMapper.java
+++ src/main/java/com/practice/response_service/web/mapper/GRPCUserMapper.java
@@ -2,7 +2,9 @@ package com.practice.response_service.web.mapper;
 
 import com.practice.grpccommon.UserProto;
 import com.practice.response_service.domain.User;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface GRPCUserMapper extends Mappable<User, UserProto>{
 
 }
